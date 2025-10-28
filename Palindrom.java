5fa3c2cd1859dac23149576c317a6670b5bd177d
Abhimanyu <abhimanyuvishwakarma4@gmail.com>
2025-10-28 14:18:13 +0530

    Second Progrma

---
+++ Palindrom.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Palindrom {
+
+    
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter: ");
+        String str = sc.nextLine();
+        
+        String result = (reverseString(str))?"Yes this is a Palindrom":" No this ia not Palindrom";
+        
+        System.out.printf("Output: %s", result);
+        
+        sc.close();
+    }
+
+    public static  boolean reverseString(String str){
+
+        String temp = str, reverse ="";
+
+        for(int i = 0; i< temp.length();i++){
+
+            char ch = temp.charAt(i);
+
+            reverse = ch+reverse;
+        }
+
+        System.out.printf("Reverse String: %s\n",reverse);
+
+        return (str.equalsIgnoreCase(str))?true:false;
+
+    }
+
+
+    
+}
